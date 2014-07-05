e9e672a3148e15d8bef3ccd51166b5478ec5a9b8
NightmaresSeller <nightmareseller@gmail.com>
2014-07-05 23:07:14 +0300

    Реалізовано:

--- TextQuest/src/com/nightmaresseller/devtests/textquest/TextQuestLauncher.java
+++ TextQuest/src/com/nightmaresseller/devtests/textquest/TextQuestLauncher.java
@@ -1,10 +1,12 @@
 package com.nightmaresseller.devtests.textquest;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class TextQuestLauncher {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
 	}
 
 }
